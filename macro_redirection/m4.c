13f9527767a55b83bdc8088ad6db15d56f243658
Gregor Reitzenstein <dean4devil@paranoidlabs.org>
2017-11-16 08:14:59 +0100

    Sooo yeah... C supports unicode. Yay \o/

---
+++ macro_redirection/m4.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#define 〇(o , ο , о , ಂ) о ## ο ## ಂ## o 
+〇(d,o,v,i)౦(〇(r,h,c,a)*p){〇(r,h,c,a)*р=p;〇(o,d,,){〇(r,tc,pu,ha)((*р==0x4)?0xa:(*р-0x0f));}
+〇(e,i,wh,l)(*++р);}〇(t,n,i,)〇(n,a,m,i)(){౦("P\x81t/\x88~\x84/wp\x85x}v/u\x84}/\x88t\x83N0\x4");}
